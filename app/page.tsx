68a7dc7792ffda3855f14d6d26b021d2e609c776
Nirpendra Chaudhary <55339698+Nirpendra09@users.noreply.github.com>
2024-11-21 03:48:21 +0000

    inital setup + auth layout

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand font-bold">StoreIt - The only storage solution you need</h1>
+
+    </div>
+  );
+}
